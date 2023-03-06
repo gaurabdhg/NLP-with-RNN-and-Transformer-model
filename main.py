9f757f69b19c1642812a23fae40b5661dfbfd2bc
gaurabdhg <77386673+gaurabdhg@users.noreply.github.com>
2023-03-06 16:42:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -155,29 +155,3 @@ for epoch in range(num_epochs):
       vacc.append(val_acc)
 
 print(f"Epoch: [{epoch+1}], Step: [Validation], Loss: {valid_loss:.4f}")
-
-import matplotlib.pyplot as plt
-
-# tacc and tloss are lists that contain the training accuracy and loss at each iteration
-
-plt.plot(range(len(tacc)), tacc,'r', label='Training accuracy')
-
-plt.plot(range(len(tloss)), tloss,'b', label='Training loss')
-
-plt.legend()
-plt.xlabel('Iteration')
-plt.ylabel('Accuracy/Loss')
-
-plt.savefig('train.png',dpi=360)
-plt.show()
-
-plt.plot(range(len(vacc)), vacc,'r', label='Valid accuracy')
-
-plt.plot(range(len(vloss)), vloss,'b', label='Valid loss')
-
-plt.legend()
-plt.xlabel('Iteration')
-plt.ylabel('Accuracy/Loss')
-
-plt.savefig('train.png',dpi=360)
-plt.show()
